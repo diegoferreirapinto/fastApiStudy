444afe4632039f2a0390946150d34702b0c1beb9
Diego Ferreira <tecdiegoferreirapinto@gmail.com>
2024-03-06 20:59:41 -0300

    init: this is a fist commit

---
+++ Exercises/payment-tax.py
@@ -0,0 +1,6 @@
+amount_money = 50
+item = 15
+tax = 1.03
+money_left = amount_money - (item * tax)
+
+print("Money left ->", money_left)
---
+++ Variables/Variables.py
@@ -0,0 +1,30 @@
+"""
+Variables
+"""
+cost = 100
+tx_percent = .25
+tx_percent = cost * tx_percent
+price = cost + tx_percent
+#print(price)
+
+"""
+[STRING]
+Can be passed in single quotes or double quotes
+"""
+
+username = "Ferreira Pinto"
+first_name = 'Diego'
+
+#print(username + " " + first_name)
+
+"""
+[COMMENTS] Or double quotes 
+"""
+
+'''
+[COMMENTS] Single quotes 
+'''
+print("Hello World!")
+print("Hi Diego")
+
+# -> Using '#' in front of each line code, this will be commented
