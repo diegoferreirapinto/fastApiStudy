be4c941448ebb74aa0ea9fc65813f75b4367fc4e
Diego Ferreira <tecdiegoferreirapinto@gmail.com>
2024-03-06 22:27:28 -0300

    exercises: getting user input and displaying it

---
+++ Input/UserInput.py
@@ -0,0 +1,9 @@
+"""
+User Input
+"""
+
+first_name = input("Enter your first name: ");
+days = input("How many days before your birth: ")
+print(first_name);
+print(f"Hi {first_name}! How r"
+      f" u?");
