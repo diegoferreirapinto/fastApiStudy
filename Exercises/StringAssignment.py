4d0dc900a0aedb171c9de071a64a75a2bb182ab3
Diego Ferreira <tecdiegoferreirapinto@gmail.com>
2024-03-06 22:39:44 -0300

    exercise: testing usefull functions to work with numbers

--- Exercises/StringAssignment.py
+++ Exercises/StringAssignment.py
@@ -1,4 +1,12 @@
 days_until_birth = input("How many days before your birthday? ")
+#
+# We can make an explicit parse on the INPUT, not only on
+#days_until_birth = int(input("How many days before your birthday? "))
+#
+#We can round a number value to approximate number
+#print(round(days_until_birth/7,2)) -> how many decimal plates we want,
+#                                      in this case is 2
+#
 #convert_days_in_weeks = (int(days_until_birth) % 7)
 
 if (int(days_until_birth) % 7 == 0):
@@ -10,4 +18,5 @@ else:
     print(f"{weeks} weeks and {days_left} days to your birthday")
 
 
+## type() is a function to verify the type of variable
 
