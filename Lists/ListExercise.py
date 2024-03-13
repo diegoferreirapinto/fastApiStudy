1e0b0aaf22ed03bc71a8e874c2478f441f36ab0d
Diego Ferreira <tecdiegoferreirapinto@gmail.com>
2024-03-12 23:42:20 -0300

    exercise: testing list native functions

---
+++ Lists/ListExercise.py
@@ -0,0 +1,15 @@
+zoo = ['dog', 'cat', 'cow', 'bird', 'elephant']  # create a list of 5 animals called zoo
+print(zoo)
+
+zoo.pop(2)  # delete the third position
+print(zoo)
+
+zoo.append('bat')  # append a new animal at the end of the list
+print(zoo)
+
+zoo.pop(0)  # delete the animal at the beginning of the list.
+print(zoo)
+
+print(zoo)  # print all the animals
+
+print(zoo[0:3])  # print only the first 3 animals
