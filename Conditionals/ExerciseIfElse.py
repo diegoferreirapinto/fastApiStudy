2c8ba0df9c943a4e94c62aca0aa743ff76f33dc5
Diego Ferreira Pinto <tecdiegoferreirapinto@gmail.com>
2024-03-14 12:51:39 -0300

    to_study: if/elif/else stament test and one exercise

---
+++ Conditionals/ExerciseIfElse.py
@@ -0,0 +1,30 @@
+""""
+    [If Else Assignment]
+
+-   Create a variable grade holding an integer between 0 - 100
+-   Code if, elif, else statements to print the letter grade of
+    the number grade variable
+
+Grades:
+
+A = 90 - 100
+B = 80 - 89
+C = 70-79
+D = 60 - 69
+F = 0 - 59
+
+Example:
+if grade = 87 then print('B')'
+"""
+grade = 89.9;
+
+if (grade >= 90):
+    print("A")
+elif (grade >= 80):
+    print("B")
+elif (grade >= 70):
+    print("C")
+elif (grade >= 60):
+    print("D")
+else:
+    print("F")
---
+++ Conditionals/IfElse.py
@@ -0,0 +1,15 @@
+""""
+Flow Control: If / Elif / Else
+"""
+
+hour = 18
+
+if (hour >= 18):
+    print(f"{hour} if")
+    print("Boa noite!")
+elif (hour >= 12):
+    print(f"{hour} if")
+    print("Boa tarde!!")
+else:
+    print(f"{hour} if")
+    print("Bom dia!!! :)")
